a59091ebea49dd6de32d9905945d084f8e6ebd44
cse18107 <cse18107@cemk.ac.in>
2024-05-26 16:33:11 +0530

    added initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import "./App.css";
-import { SplitScreen } from "./components/screen-splitter";
-
-const LeftSideComp = () => {
-  return <h2 style={{ backgroundColor: "crimson" }}>I am Left!</h2>;
-};
-
-const RightSideComp = () => {
-  return <h2 style={{ backgroundColor: "orange" }}>I am Right!</h2>;
-};
 
 function App() {
-  return (
-    <SplitScreen
-      Left={LeftSideComp}
-      Right={RightSideComp}
-      leftWidth={1}
-      rightWidth={3}
-    />
-  );
+  return <></>;
 }
 
 export default App;
