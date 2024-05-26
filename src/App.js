7a66d9503ac4c1a3436e00f4c47283d4c7dd284d
cse18107 <cse18107@cemk.ac.in>
2024-05-26 16:29:44 +0530

    added content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import "./App.css";
 import { LargeAuthorListItem } from "./components/authors/LargeListItems";
 import { SmallAuthorListItem } from "./components/authors/SmallListItems";
+import { LargeBookListItem } from "./components/books/LargeListItems";
+import { SmallBookListItem } from "./components/books/SmallListItems";
 import { RegularList } from "./components/list/Regular";
 
 import { authors } from './data/author';
+import { books } from './data/books';
 
 
 
 function App() {
   return (
     <>
-      <RegularList items={authors} sourceName={"author"} ItemComponent={SmallAuthorListItem}/>
-      <RegularList items={authors} sourceName={"author"} ItemComponent={LargeAuthorListItem}/>
+      {/* <RegularList
+        items={authors}
+        sourceName={"author"}
+        ItemComponent={SmallAuthorListItem}
+      />
+      <RegularList
+        items={authors}
+        sourceName={"author"}
+        ItemComponent={LargeAuthorListItem}
+      /> */}
+      <RegularList
+        items={books}
+        sourceName={"book"}
+        ItemComponent={SmallBookListItem}
+      />
+      <RegularList
+        items={books}
+        sourceName={"book"}
+        ItemComponent={LargeBookListItem}
+      />
     </>
   );
 }
